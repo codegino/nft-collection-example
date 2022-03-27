83b42882b115dc691864db06e2ffba3c7399e0aa
codegino <carloginocatapang@gmail.com>
2022-03-27 12:13:30 +0800

    feat: Implemented smart contract for NFT collection

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x60664425eBdD612B306B6f007D09971D050b389B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
